fde4adcb501cac1131017f7d15dcf344aa3d9e41
Thiago <thsethub@gmail.com>
2026-01-13 19:26:27 -0300

    feat: Configurar deploy para GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  basePath: '/nobre-matilha',
   experimental: {
     // Habilita a pasta src/app
   },
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
