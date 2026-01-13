fcc19d8ea08d74a8bd3737c81701693b918468ed
Thiago <thsethub@gmail.com>
2026-01-13 19:49:51 -0300

    Comentando o basePath para funcionar com o domínio personalizado.

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export',
-  basePath: '/nobre-matilha',
+  output: "export",
+  // basePath: "/nobre-matilha",
   experimental: {
     // Habilita a pasta src/app
   },
